2c5afb24b804282646177ec076577b79b56ca9f4
ayasuda <ayasuda@AtsushiYasuda-spirits-virtual-machine.cyberagent.co.jp>
2012-01-13 10:54:57 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#dotfiles installer
+
+install_path=$HOME
+installer_path=`pwd`
+
+echo <<DESCRIPTION
+installing those files
+	.vimrc
+	.vim (directory)
+	.screenrc
+	.zshrc
+DESCRIPTION
+
+install_file()
+{
+	local _target=$1
+	local _linkname=$2
+	if [ ! -e "${_linkname}" ]; then
+		ln -s "${_target} ${_linkname}"
+  fi
+}
+
+install_file "${installer_path}/.vimrc" "${install_path}/.vimrc"
+install_file "${installer_path}/.vim" "${install_path}/.vim"
+install_file "${installer_path}/.screenrc" "${install_path}/.screenrc"
+install_file "${installer_path}/.zshrc" "${install_path}/.zshrc"
